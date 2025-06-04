752eedc535713b8ea0e061056bf7d47219927541
Harshit-Wadichar <manohar.wadichar9545@gmail.com>
2025-06-04 19:08:17 +0530

    adding 02counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+ //const counter = 5;
+ const addValue = () => {
+  if(count<20){
+  setCount(count + 1);
+  }
+  console.log("addValue called", count);
+ }
+
+ const removeValue = () => {
+  if(count>0){
+  setCount(count - 1);
+  }
+  console.log("removeValue called", count);
+ }
+
+  return (
+    <>
+      <h1> chai aur react</h1>
+      <h2>counter value: {count}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+    </>
+  );
+}
+
+export default App;
